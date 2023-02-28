b99c80f65ef5a358fbe7e507ec048e9e2784b3fa
MGBEOKWERE CHIMDINDU <cmgbeokwere6@gmail.com>
2023-02-28 01:49:01 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// https://api.adviceslip.com/advice
+
+let adviceText = document.querySelector(".advice__text");
+let adviceId = document.querySelector("#advice__id");
+const button = document.querySelector(".button");
+
+async function getAdvice() {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+
+    const data = await response.json();
+
+    const { id, advice } = data.slip;
+
+    console.log(advice, id);
+
+    adviceText.textContent = advice;
+    adviceId.textContent = "#" + id;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+button.addEventListener("click", getAdvice);
